4ee26873717d263ac67553ba89578e526f6d672a
miyaajd <ehdal1032@gmail.com>
2025-08-16 18:54:47 +0900

    section-home js done

--- js/home.js
+++ js/home.js
@@ -4,4 +4,9 @@ window.addEventListener("load", function () {
     slidesPerView: 4.5,
     spaceBetween: 20
   });
+  const homeSwiper2 = new Swiper(".tablet-h-right", {
+    loop: true,
+    direction: "vertical",
+    slidesPerView: 3.5,
+  });
 });
