29665944e1c1c5f7c632c0b282bed16e6f5dbcf3
miyaajd <ehdal1032@gmail.com>
2025-08-16 19:22:21 +0900

    js done.

--- js/perfume.js
+++ js/perfume.js
@@ -3,6 +3,10 @@ window.addEventListener("load", function () {
   const thumbWrap = this.document.querySelector(
     ".perfume-bottom .perfume-list"
   );
+  const tabletMainImg = this.document.querySelector("#hinoki-p");
+  const tabletThumbWrap = this.document.querySelector(
+    ".tablet-p-bottom .tablet-p-list"
+  );
   const thumbimg = thumbWrap ? thumbWrap.querySelectorAll("img") : [];
   //
   if (mainImg.length < 2 || !thumbWrap || thumbimg.length === 0) return;
@@ -27,4 +31,21 @@ window.addEventListener("load", function () {
       mainImg.forEach((images) => (images.style.opacity = "1"));
     }, 150);
   });
+  // ************768**************
+  if (!tabletMainImg || !tabletThumbWrap) return;
+  if (tabletMainImg && tabletThumbWrap) {
+    tabletMainImg.style.transition = "opacity .15s ease";
+    tabletThumbWrap.addEventListener("click", (e) => {
+      const img = e.target.closest("img");
+      if (!img) return;
+      tabletMainImg.src = img.src;
+      tabletMainImg.style.opacity = "0";
+      setTimeout(() => {
+        tabletMainImg.src = img.src;
+        requestAnimationFrame(() => {
+          tabletMainImg.style.opacity = "1";
+        });
+      }, 150);
+    });
+  }
 });
