92f1ca67bd086bdcd868f4ba430bf32a2442de72
miyaajd <ehdal1032@gmail.com>
2025-08-19 00:46:32 +0900

    상세페이지1 build

--- js/blueHinoki.js
+++ js/blueHinoki.js
@@ -1,1 +1,13 @@
-
+window.addEventListener("load", function () {
+  const blueSwiper = new Swiper(".blue-left", {
+  navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+  const blueSwiper2 = new Swiper(".perfumes", {
+    loop: true,
+    slidesPerView: 5.5,
+    spaceBetween: 8
+  });
+});
