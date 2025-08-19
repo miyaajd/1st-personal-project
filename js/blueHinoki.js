2e74f88be9fa75c51925e266295164a4645e8f96
miyaajd <ehdal1032@gmail.com>
2025-08-19 13:34:35 +0900

    blueHinoki js done

--- js/blueHinoki.js
+++ js/blueHinoki.js
@@ -1,13 +1,22 @@
 window.addEventListener("load", function () {
   const blueSwiper = new Swiper(".blue-left", {
-  navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   });
   const blueSwiper2 = new Swiper(".perfumes", {
     loop: true,
     slidesPerView: 5.5,
-    spaceBetween: 8
+    spaceBetween: 8,
+  });
+  //
+  // 사이즈 선택
+  const sizeOption = this.document.querySelectorAll(".mL > li");
+  sizeOption.forEach((size) => {
+    size.addEventListener("click", () => {
+      sizeOption.forEach((li) => li.classList.remove("active"));
+      size.classList.add("active");
+    });
   });
 });
