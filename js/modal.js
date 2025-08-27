930799fadff681c12d5beeed420d76e32421e107
miyaajd <ehdal1032@gmail.com>
2025-08-27 12:56:53 +0900

    cursor edit

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function () {
   const modal = document.querySelector("#myModal");
   const closeBtn = document.querySelector("#closeBtn");
-  const eventBtn = this.document.querySelector("#event")
+  const eventBtn = this.document.querySelector("#event");
   //
   modal.style.display = "block";
   //닫기
@@ -9,16 +9,18 @@ window.addEventListener("load", function () {
     modal.style.display = "none";
   });
   // 버튼
-  eventBtn.addEventListener("click" , function(){
-    window.location.href = "bluehinoki.html"
-  })
-  // 
-  // 
+  eventBtn.addEventListener("click", function () {
+    window.location.href = "bluehinoki.html";
+  });
+  //
+  //
   // read me modal
-   // modal
+  // modal
+  this.document.documentElement.style.overflow = "hidden";
   const modalClose = this.document.querySelector("#modalClose");
   const modal2 = this.document.querySelector(".modalWrap");
   modalClose.addEventListener("click", () => {
     modal2.classList.add("none");
+    this.document.documentElement.style.overflow = "";
   });
 });
